a2f5a2839c42a8a8affecaeeee035460555274a7
Arian Abedanzadeh <arianabedan@gmail.com>
2021-07-20 12:41:16 -0400

    New File

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([4,3,8], [2,7,3]), true);
