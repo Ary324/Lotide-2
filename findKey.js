5cf18dcd6b759cdf78d0c6ba3691da32f76c4609
Arian Abedanzadeh <arianabedan@gmail.com>
2021-07-23 18:43:42 -0400

    New File

---
+++ findKey.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  
+  let foundTheKey;
+
+  for (let key in object) {
+     
+    if (callback(object[key])) {
+      foundTheKey = key;
+      return foundTheKey;
+    } else {
+      continue;
+    }
+  }
+  return undefined;
+};
+
+
+
+const code = "noma";
+const code2 = "Akaleri";
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), code);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), code2);
