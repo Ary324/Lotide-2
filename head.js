eaa3ffb97f8023cdf4d0d1eb919cdedbed28949c
Arian Abedanzadeh <arianabedan@gmail.com>
2021-07-26 13:32:56 -0400

    Refactored

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const head = function(values) {
   return values[0];
 };
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
